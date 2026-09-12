0669490bd997d2e8e247e10212fe031e1823ce37
rihaab06 <rihaabwadekar0605@gmail.com>
2026-09-12 18:47:39 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -1,14 +1,7 @@
-from dotenv import load_dotenv
+import streamlit as st
 from langchain_groq import ChatGroq
-import os
-
-load_dotenv()
 
 llm = ChatGroq(
-    groq_api_key=os.getenv("GROQ_API_KEY"),
+    groq_api_key=st.secrets["GROQ_API_KEY"],
     model_name="openai/gpt-oss-120b"
 )
-
-if __name__ == "__main__":
-    response = llm.invoke("What are the two main ingredients in samosa?")
-    print(response.content)
